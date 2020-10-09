721681393395d986e23cb86787e6a437dc839f30
aucky aisy <52421145+auckyaisy@users.noreply.github.com>
2020-10-09 21:46:26 +0700

    Update urls.py

--- kpuvsmala/urls.py
+++ kpuvsmala/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pemilu.urls')),
-    url('^$', include('loaderio.urls'))
+    path('', include('pemilu.urls'))
 ]

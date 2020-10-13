e62e69d861a99b6c5f2d23eaa5938f94e014e606
aucky aisy <52421145+auckyaisy@users.noreply.github.com>
2020-10-14 00:48:36 +0700

    cek

--- kpuvsmala/settings.py
+++ kpuvsmala/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^zh$@rw%6(#xw&-8$o)zx!-9co(8pd+08%*h8d&lb)^4))k4s^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+APPEND_SLASH = False
 
 ALLOWED_HOSTS = [
     '50a6144717a7.ngrok.io',

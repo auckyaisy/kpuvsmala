eb3e5e9448b940bf86c8e8baae9dada35b85851d
aucky <auckyqq@gmail.com>
2020-11-06 15:43:25 +0700

    buka lagi

--- pemilu/admin.py
+++ pemilu/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile, User, Pilih
+from .models import UserProfile, User
 
 
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(User)
-admin.site.register(Pilih)
+# admin.site.register(Pilih)
